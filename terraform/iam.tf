23f6b9eaa24fc05341e77ba3f126e4cf5a7bd37f
AungMawMaw <aungmaw.bc@gmail.com>
2024-04-11 21:05:12 +0630

    init iam for lambda

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,12 +23,14 @@ resource "aws_iam_role_policy_attachment" "attach_exec_role" {
   role = aws_iam_role.lambda_main.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+#end of lambda
 
 #create iampolicy
 data "aws_iam_policy_document" "lambda_websocket" {
    statement {
+    effect = "Allow"
        actions = [
-      "execute-api:ManageConnects",
+      "execute-api:ManageConnection",
       "dynamodb:DescribeTable",
       "dynamodb:Scan",
       "dynamodb:GetItem",
@@ -42,42 +44,18 @@ data "aws_iam_policy_document" "lambda_websocket" {
            "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_queue_name}",
            "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${var.websocket_table_name}",
            "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${var.vendor_table_name}",
+      ### TODO: websocket
        ]
    }
 
-   statement {
-       actions = [
-           "s3:ListBucket",
-       ]
-       resources = [
-           "arn:aws:s3:::${var.s3_bucket_name}",
-       ]
-       condition {
-           test = "StringLike"
-           variable = "s3:prefix"
-           values = [
-               "",
-               "home/",
-               "home/&{aws:username}/",
-           ]
-       }
-   }
-
-   statement {
-       actions = [
-           "s3:*",
-       ]
-       resources = [
-           "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
-           "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
-       ]
-   }
 }
 
-resource "aws_iam_policy" "example" {
-   name = "example_policy"
-   path = "/"
-   policy = "${data.aws_iam_policy_document.example.json}"
+resource "aws_iam_policy" "lambda_websocket" {
+   name = "${var.app_name}-lambda-websocket"
+   policy = "${data.aws_iam_policy_document.lambda_websocket.json}"
 }
 
-
+resource "aws_iam_role_policy_attachment" "lambda_websocket" {
+  policy_arn = aws_iam_policy.lambda_websocket.arn
+  role = aws_iam_role.lambda_main.name
+}
